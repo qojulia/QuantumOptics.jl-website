6ca308d0559877672615c13872c7a3922a465aa8
Sebastian Kraemer <basti.kr@gmail.com>
2017-04-12 14:57:29 +0200

    Update benchmarks
    * Compare against QuTiP 4.1
    * Add timedependent example
    * Group benchmarks in related sets

--- benchmarks.php
+++ benchmarks.php
@@ -8,28 +8,50 @@
                 <h1 class="page-header">
                    Benchmarks
                 </h1>
-                
+
                 <p>Benchmark tests are conducted with the following set of test programs found on the <a href="https://github.com/bastikr/QuantumOpticsFrameworks-Benchmarks">Quantum Optics Frameworks Benchmark Repo</a></p>.
             </div>
         </div>
-        <div class="row"> 
-            <div class="col-md-8 col-md-offset-2">
+        <div class="row">
+            <div class="col-md-6">
             	<canvas id="plot-timeevolution-master"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="plot-timeevolution-particle"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="plot-timeevolution-timedependent"></canvas>
             </div>
         </div>
         <div class="row">
-	        <div class="col-md-6">
-            	<canvas id="plot-timeevolution-particle"></canvas>
+            <div class="col-md-6">
+            	<canvas id="plot-multiplication-sparse-sparse"></canvas>
             </div>
             <div class="col-md-6">
-            	<canvas id="plot-multiplication"></canvas>
+                <canvas id="plot-multiplication-sparse-dense"></canvas>
             </div>
+            <div class="col-md-6">
+                <canvas id="plot-multiplication-dense-sparse"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="plot-multiplication-dense-dense"></canvas>
+            </div>
+        </div>
+        <div class="row">
             <div class="col-md-6">
             	<canvas id="plot-expect-state"></canvas>
             </div>
 	        <div class="col-md-6">
             	<canvas id="plot-expect-operator"></canvas>
             </div>
+            <div class="col-md-6">
+                <canvas id="plot-variance-state"></canvas>
+            </div>
+            <div class="col-md-6">
+                <canvas id="plot-variance-operator"></canvas>
+            </div>
+        </div>
+        <div class="row">
              <div class="col-md-6">
             	<canvas id="plot-coherentstate"></canvas>
             </div>
@@ -42,13 +64,8 @@
 	        <div class="col-md-6">
             	<canvas id="plot-qfunc-operator"></canvas>
             </div>
-             <div class="col-md-6">
-            	<canvas id="plot-variance-state"></canvas>
-            </div>
-	        <div class="col-md-6">
-            	<canvas id="plot-variance-operator"></canvas>
-            </div>
+
         </div>
     </div>
-           
+
  <?php include('footer.html'); ?>
--- js/qojl.js
+++ js/qojl.js
@@ -1,18 +1,18 @@
 // Config
 var qojl_data = "QuantumOptics.jl-ae69320f5c";
-var qutip_data = "QuTiP-3.1.0";
+var qutip_data = "QuTiP-4.1.0";
 var toolbox_data = "QuantumOpticsToolbox";
 
 // Rearrange dat structure
 function rearrange (jsondata) {
 		d = [];
-	
+
 	for (var i in jsondata) {
 		xval = jsondata[i].N;
 		yval = jsondata[i].t;
 		d.push({x: xval, y: yval});
 	}
-	
+
 	return d;
 }
 
@@ -29,7 +29,7 @@ function chartconfig (data, charttitle) {
 		       borderColor: '#d66761',
 		       backgroundColor: '#d66761'
 	        }, {
-	            label: 'QuTiP 3.1.0',
+	            label: 'QuTiP 4.1.0',
 	            data: rearrange(data[qutip_data]),
 	            fill: false,
 	            borderWidth: 3.0,
@@ -56,14 +56,14 @@ function chartconfig (data, charttitle) {
 	                scaleLabel: {
 		                display: true,
 		                labelString: 'Hilbert-Space Dimension'
-	                }      
+	                }
 	            }],
 	            yAxes: [{
 	                scaleLabel: {
 		                display: true,
 		                labelString: 'Ellapsed Time'
 	                }
-	                
+
 	            }]
 	        }
 	    }
@@ -82,12 +82,20 @@ function createplot (file, htmlid, title) {
 // Now, create the plots
 createplot('timeevolution_master.json', '#plot-timeevolution-master', 'Time Evolution (Master Equation)');
 createplot('timeevolution_particle.json', '#plot-timeevolution-particle', 'Time Evolution (Particle)');
-createplot('multiplication.json', '#plot-multiplication', 'Multiplication');
+createplot('timeevolution_timedependent.json', '#plot-timeevolution-timedependent', 'Time Evolution (Time dependent)');
+
+createplot('multiplication_sparse_sparse.json', '#plot-multiplication-sparse-sparse', 'Multiplication: sparse-sparse');
+createplot('multiplication_sparse_dense.json', '#plot-multiplication-sparse-dense', 'Multiplication: sparse-dense');
+createplot('multiplication_dense_sparse.json', '#plot-multiplication-dense-sparse', 'Multiplication: dense-sparse');
+createplot('multiplication_dense_dense.json', '#plot-multiplication-dense-dense', 'Multiplication: dense-dense');
+
 createplot('expect_state.json', '#plot-expect-state', 'Expectation Value (State Vector)');
 createplot('expect_operator.json', '#plot-expect-operator', 'Expectation Value (Density Operator)');
-createplot('coherentstate.json', '#plot-coherentstate', 'Coherent State Performance');
-createplot('ptrace.json', '#plot-ptrace', 'Partial Trace Performance');
-createplot('qfunc_state.json', '#plot-qfunc-state', 'Q-Function for State Vectors');
-createplot('qfunc_operator.json', '#plot-qfunc-operator', 'Q-Function for Density Operators');
 createplot('variance_operator.json', '#plot-variance-operator', 'Variance (Density Operator)');
 createplot('variance_state.json', '#plot-variance-state', 'Variance (State Vector)');
+
+createplot('ptrace.json', '#plot-ptrace', 'Partial Trace Performance');
+
+createplot('coherentstate.json', '#plot-coherentstate', 'Coherent State Performance');
+createplot('qfunc_state.json', '#plot-qfunc-state', 'Q-Function for State Vectors');
+createplot('qfunc_operator.json', '#plot-qfunc-operator', 'Q-Function for Density Operators');
