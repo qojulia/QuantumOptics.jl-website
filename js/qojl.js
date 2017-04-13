59741e13bbf1dcc375195f681b5b7cecc73c5192
Sebastian Kraemer <basti.kr@gmail.com>
2017-04-13 09:01:54 +0200

    Implement tabs for benchmarks.

--- js/qojl.js
+++ js/qojl.js
@@ -70,6 +70,26 @@ function chartconfig (data, charttitle) {
 	};
 }
 
+function openBenchmarkSet(evt, benchmarkName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(benchmarkName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
 // Function to create Plot from JSON
 function createplot (file, htmlid, title) {
