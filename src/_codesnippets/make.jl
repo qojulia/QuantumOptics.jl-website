4ad76e30350afe314412a83c3a5d963a0b6243f8
Sebastian Kraemer <basti.kr@gmail.com>
2017-05-04 17:16:12 +0200

    Improve codesippets.

--- src/_codesnippets/make.jl
+++ src/_codesnippets/make.jl
@@ -1,3 +1,18 @@
-using Documenter
+sourcedir = "src"
+builddir = "build"
+imagedir = "../../images/codesnippets"
 
-makedocs()
+cp(sourcedir, builddir; remove_destination=true)
+cd(builddir)
+
+names = filter(name->endswith(name, ".jl"), readdir("."))
+for name in names
+    println("Executing $name")
+    run(`julia $name`)
+end
+
+imagenames = filter(name->endswith(name, ".svg")||endswith(name, ".png"), readdir("."))
+for name in imagenames
+    println("Copying $name")
+    cp(name, joinpath(imagedir, name); remove_destination=true)
+end
