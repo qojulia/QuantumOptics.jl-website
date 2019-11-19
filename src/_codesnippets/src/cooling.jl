c7f8ea1af6032f6d3cad8a7c309880c2e7657e13
David Plankensteiner <david.plankensteiner@gmx.at>
2019-11-19 16:04:06 +0100

    Update code-snippets and add them to front page

--- src/_codesnippets/src/cooling.jl
+++ src/_codesnippets/src/cooling.jl
@@ -3,21 +3,20 @@ bc = FockBasis(16)
 ba = SpinBasis(1//2)
 a = destroy(bc) ⊗ one(ba)
 sm = one(bc) ⊗ sigmam(ba)
-H0 = 0.5*dagger(sm)*sm + a + dagger(a)
-Hx = 0.5*(dagger(a)*sm + a*dagger(sm))
+H0 = 0.5*sm'*sm + a + a'
+Hx = 0.5*(a'*sm + a*sm')
 J = [a, sqrt(2)*sm]
-Jdagger = map(dagger, J)
+Jdagger = dagger.(J)
 
-function fquantum(t, ψ, u)
-  H0 + Hx*cos(real(u[1])), J, Jdagger
-end
+fquantum(t, ψ, u) = H0 + Hx*cos(u[1]), J, Jdagger
 function fclassical(t, ψ, u, du)
   du[1] = 0.3*real(u[2])
   du[2] = sin(real(u[1]))*real(expect(dagger(a)*sm, ψ))
 end
-ψ0 = semiclassical.State(fockstate(bc, 0) ⊗ spindown(ba), ComplexF64[sqrt(2), 6.0])
-tout, ρt = semiclassical.master_dynamic([0:0.1:200;], ψ0, fquantum, fclassical)
-p = [real(r.classical[2]) for r=ρt]
+u0 = ComplexF64[sqrt(2), 6.0]
+ψ0 = semiclassical.State(fockstate(bc, 0)⊗spindown(ba), u0)
+tout, p = semiclassical.master_dynamic([0:1.0:200;], ψ0, fquantum,
+      fclassical; fout=(t,rho)->rho.classical[2])
 
 using PyPlot
 plot(tout, p.^2)
--- src/_codesnippets/src/gross-pitaevskii.jl
+++ src/_codesnippets/src/gross-pitaevskii.jl
@@ -6,21 +6,18 @@ Txp = transform(b_x, b_p)
 x = position(b_x)
 p = momentum(b_p)
 
-Hkin = LazyProduct(Txp, p^2/4, Tpx)
-V = 0.2*x^2
+Hkin = LazyProduct(Txp, p^2/2, Tpx)
 Hpsi = diagonaloperator(b_x, Ket(b_x).data)
-H = LazySum(Hkin, V, Hpsi)
+H = LazySum(Hkin, Hpsi)
 
-function fout(t, ψ)
-    Hpsi.data.nzval .= -abs2.(ψ.data)
-    H
-end
-ψ₀ = gaussianstate(b_x, 0, 0, 0.7)
-tout, ψₜ = timeevolution.schroedinger_dynamic([0:0.1:20;], ψ₀, fout)
+fquantum(t, ψ) = (Hpsi.data.nzval .= -50 .* abs2.(ψ.data); H)
+ψ₀ = gaussianstate(b_x,-2π,2,1.5) + gaussianstate(b_x,2π,-2,1.5)
+normalize!(ψ₀)
+tout, ψₜ = timeevolution.schroedinger_dynamic([0:0.01:6;], ψ₀, fquantum)
+density = [abs2(ψ.data[j]) for ψ=ψₜ, j=1:length(b_x)]
 
 using PyPlot
-plot(samplepoints(b_x), abs2.(ψₜ[1].data))
-plot(samplepoints(b_x), abs2.(ψₜ[end].data))
+contourf(samplepoints(b_x),tout,density,50)
 xlabel("x")
-ylabel("Density")
-savefig("gross_pitaevskii.svg")
+ylabel("Time")
+savefig("gross_pitaevskii.png")
